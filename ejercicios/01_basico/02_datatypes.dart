3eba4770b80a2e72e6b95e7e7530074180e6065b
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-07 18:48:43 -0300

    Tipado de datos: Sets

--- ejercicios/01_basico/02_datatypes.dart
+++ ejercicios/01_basico/02_datatypes.dart
@@ -3,14 +3,10 @@ main() {
   // === Números 
   int a = 10;
   double b = 5.5;
-
   int? c;
-
   int _a = 30;
   double $b = 40;
-
   double resultado = _a + $b;
-
   // print(resultado);
 
   // === String 
@@ -19,28 +15,36 @@ main() {
   String nombre3 = "O'Connor";
   String apellido = 'Stark';
   String nombreCompleto = '$nombre $apellido';
-
   String multilinea = '''
   Hola Mundo
   ¿Cómo estás?
   $nombreCompleto
   O'Connor''';
-
   // print(multilinea);
 
   // === Booleans
   bool isActive = true;
   bool isNotActive = !isActive;
-
   // print( isNotActive );
 
   // === Lists
   // List<String> villanosDeprecated = new List();
   List<String> villanos = ['Lex', 'Red Skull', 'Doom'];
+  villanos.add('Duende Verde');
+  villanos.add('Duende Verde');
+  villanos.add('Duende Verde');
+  villanos.add('Duende Verde');
+  // print(villanos);
+  // var villanosSet = villanos.toSet();
+  // print(villanosSet.toList());
 
-  print(villanos);
-
-
+  // === Sets
+  Set<String> villanos2 = {'Lex', 'Red Skull', 'Doom'};
+  villanos2.add('Duende Verde');
+  villanos2.add('Duende Verde');
+  villanos2.add('Duende Verde');
+  villanos2.add('Duende Verde');
+  // print(villanos2);
 
 
 }
