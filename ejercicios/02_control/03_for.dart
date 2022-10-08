01dbad3d563d90f0a29d0e911a02c26216007ea2
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-08 09:13:24 -0300

    Ciclo for

---
+++ ejercicios/02_control/03_for.dart
@@ -0,0 +1,23 @@
+main() {
+
+  for(int i = 0; i <= 10; i++) {
+    print('index i: ${ 2 * 10 }');
+  }
+
+  /**
+   *  Dato de entrada: La base de la tabla de multiplicar 
+   *  (este dato debe de ser capturado por el usuario)
+   *  ej: 2     2, 4, 6, 8, 10
+   * 
+   *  La salida esperada sería
+   * 
+   *  2 * 1 = 2
+   *  2 * 1 = 4
+   *  2 * 1 = 6
+   *  ..
+   *  2 * 10 = 20
+   * 
+   */
+
+
+}
