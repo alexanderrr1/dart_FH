262ccd852ca76a07b474962ee5d635c4ae85cd31
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-08 09:03:26 -0300

    Resolución tarea IF y ELSE

--- ejercicios/02_control/02_ifelse.dart
+++ ejercicios/02_control/02_ifelse.dart
@@ -18,9 +18,20 @@ main() {
    *  Si estas entre 18 y 20 (incluyendo 18), mostrar "Mayor de edad"
    *  Si eres menos a 18 (sin contar 18), mostrar "Menor de edad"
    * 
+   *  28 Ciudadano
+   *  21 Ciudadano
+   *  20 Mayor de edad
+   *  18 Mayor de edad
+   *  17 Menos de edad
+   * 
    */
 
-  
-
+  if(edad >= 21) {
+    stdout.writeln('Ciudadano');
+  } else if(edad <= 20 && edad >= 18) {
+    stdout.writeln('Mayor de edad');
+  } else {
+    stdout.writeln('Menor de edad');
+  }
 
 }
