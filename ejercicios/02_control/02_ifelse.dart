276daafc0df0453058658a9efa99e7590a9161d8
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-08 09:00:13 -0300

    If y Else

---
+++ ejercicios/02_control/02_ifelse.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+main() {
+
+  stdout.writeln('¿Cual es tu edad?');
+  int? edad = int.parse(stdin.readLineSync() ?? '0') ;
+
+  // if( edad >= 18 ) {
+  //   stdout.writeln('Eres mayor de edad');
+  // } else {
+  //   stdout.writeln('Eres menor de edad');
+  // }
+
+  /**
+   * Crear un programa en dart que:
+   *  
+   *  Si eres mayor o igual a 21 años, mostrar la palabra "ciudadano"
+   *  Si estas entre 18 y 20 (incluyendo 18), mostrar "Mayor de edad"
+   *  Si eres menos a 18 (sin contar 18), mostrar "Menor de edad"
+   * 
+   */
+
+  
+
+
+}
