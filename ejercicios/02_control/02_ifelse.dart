cae9839a0b703d0437284120f39e0658098f9258
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-08 09:06:35 -0300

    Mejora de ejercicio de tarea

--- ejercicios/02_control/02_ifelse.dart
+++ ejercicios/02_control/02_ifelse.dart
@@ -28,7 +28,7 @@ main() {
 
   if(edad >= 21) {
     stdout.writeln('Ciudadano');
-  } else if(edad <= 20 && edad >= 18) {
+  } else if(edad >= 18) {
     stdout.writeln('Mayor de edad');
   } else {
     stdout.writeln('Menor de edad');
