505c47194c3346c99e524251ae69b5e79cd670e6
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-09 11:31:37 -0300

    Argumentos a las funciones - Posicionales

---
+++ ejercicios/03_funciones/02_argumentos.dart
@@ -0,0 +1,7 @@
+void saludar( String mensaje, [String nombre = '<insertar nombre>', int edad = 20]  ) {
+  print('$mensaje $nombre - $edad');
+}
+
+void main(List<String> args) {
+  saludar('Hola', 'Fernando', 35);
+}
