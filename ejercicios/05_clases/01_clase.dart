7c419971589e25fd76fb0588afb8a4092fd4184c
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-09 17:25:25 -0300

    Clases en archivos independientes

--- ejercicios/05_clases/01_clase.dart
+++ ejercicios/05_clases/01_clase.dart
@@ -1,38 +1,14 @@
+import 'clases/persona.dart';
+
 void main(List<String> args) {
   
-  // final persona = {
-  //   'nombre': 'Fernando',
-  //   'edad': 33,
-  //   'bio': 'Nació en Guadalajara'
-  // };
-
-  // print(persona['edad']);
-
   final persona = new Persona();
-  persona.nombre = 'Fernando';
-  persona.edad = 33;
-  persona.bio = "Nació por ahí";
 
-  print(persona.toString());
+  persona
+    ..nombre = 'Fernando'
+    ..edad = 33
+    ..bio = "Nació por ahí";
 
-}
-
-class Persona {
-
-  // Campos o propiedades
-  String? nombre;
-  int? edad;
-  String? bio;
-
-  // Getter y Setters
-
-  // Constructores
-
-  // Métodos
-  @override
-  String toString() => '$nombre $edad $bio';
+  print(persona);
 
 }
-
-
-
---
+++ ejercicios/05_clases/clases/persona.dart
@@ -0,0 +1,16 @@
+class Persona {
+
+  // Campos o propiedades
+  String? nombre;
+  int? edad;
+  String? bio;
+
+  // Getter y Setters
+
+  // Constructores
+
+  // Métodos
+  @override
+  String toString() => '$nombre $edad $bio';
+
+}
