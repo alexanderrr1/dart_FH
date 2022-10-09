aee1b5320b28dc29727621480eaef196cf15872e
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-09 11:39:49 -0300

    Argumentos a las funciones - por Nombre

--- ejercicios/03_funciones/02_argumentos.dart
+++ ejercicios/03_funciones/02_argumentos.dart
@@ -1,7 +1,15 @@
+import 'dart:math';
+
 void saludar( String mensaje, [String nombre = '<insertar nombre>', int edad = 20]  ) {
   print('$mensaje $nombre - $edad');
 }
 
+void saludar2(String? mensaje, { required String nombre, int veces = 10 }) {
+    // Cuerpo de la función
+    print('Saludar2: $mensaje $nombre - $veces');
+}
+
 void main(List<String> args) {
   saludar('Hola', 'Fernando', 35);
+  saludar2('Saludos', veces: 20, nombre: 'Tony');
 }
