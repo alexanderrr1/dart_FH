6bd8e67e5a2a16284ee42db9c278cfd2476214b3
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-09 15:39:12 -0300

    Futures

---
+++ ejercicios/04_tipos_nocomunes/03_future.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  
+  Future<String> timeout = Future.delayed( Duration(seconds: 3), () {
+    print('3 segundos despues!');
+    return 'Retorno del Future';
+  });
+
+  timeout.then(print);
+
+  print('Fin del main');
+
+}
