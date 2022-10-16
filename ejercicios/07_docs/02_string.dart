053cf4db7660c172424e61d879dfc1d457273c3c
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-16 10:25:31 -0300

    Strings - Operadores y más métodos

--- ejercicios/07_docs/02_string.dart
+++ ejercicios/07_docs/02_string.dart
@@ -17,4 +17,31 @@ void main(List<String> args) {
 
   // PadLeft
   print('PadLeft: ${nombreCompleto.padLeft(20, '...')}');
+
+  // PadRight
+  print('PadRight: ${nombreCompleto.padRight(20, '...')}');
+
+  // Operador []
+  print('Operador []: ${nombreCompleto[10]}');
+
+  // Operador *
+  print('Operador *: ${nombreCompleto * 2}');
+  print('Operador *: ${ '*' * 10}');
+
+  // ReplaceAll
+  print('ReplaceAll: ${nombreCompleto.replaceAll(new RegExp(r'e'), 'a')}');
+
+  // SubString
+  print('SubString: ${nombreCompleto.substring(0, 5)}...');
+
+  // IndexOf
+  print('IndexOf: ${nombreCompleto.indexOf('F')}');
+
+  // Split
+  print('Split: ${nombreCompleto.split(' ')}');
+  print('Split: ---${nombreCompleto.split(' ')[1]}---');
+
+  // Ejercicio - Capitalizar: Tomar la ultima letra del string de manera calculada y capitalizarla
+  print('Ejercicio: ${nombreCompleto[(nombreCompleto.length - 1)].toUpperCase()}');
+
 }
