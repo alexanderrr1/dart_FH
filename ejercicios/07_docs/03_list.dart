ee8e19108fef6e0c6d52962236ab3ee15aaf1878
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-17 09:00:23 -0300

    List: Más métodos

--- ejercicios/07_docs/03_list.dart
+++ ejercicios/07_docs/03_list.dart
@@ -24,5 +24,37 @@ void main(List<String> args) {
   Map listaMapa = lista.asMap();
   print('ListaMapa: ${listaMapa[4]}');
   Map nombreMapa = nombres.asMap();
+  print('ListaNombres: ${nombreMapa}');
   print('ListaNombres: ${nombreMapa[1]}');
+
+  // IndexOf
+  print('IndexOf: ${nombres.indexOf('Peter')}'); // En caso de no encontrar retorna -1.
+
+  // IndexWhere
+  int mayor3 = lista.indexWhere((numero) => numero > 3);
+  print('IndexWhere: ${mayor3}');
+
+  // Remove
+  print('Remove: ${nombres.remove('Tony')}'); // Modifica el listado original.
+  print('Remove: ${nombres}');
+
+  // Shuffle
+  lista.shuffle();
+  print('Shuffle: $lista');
+  
+  // Sort & Reverse
+  lista3.sort();
+  print('Sort: $lista3');
+  print('Reverse: ${lista3.reversed.toList()}'); // Sin el toList es un iterable
+
+  // ForEach
+  nombres.forEach((nombre) { 
+    nombre = nombre.toUpperCase();
+    print(nombre);
+   });
+  print('Listado: ${nombres}');
+
+  // Map
+  final newList = nombres.map((nombre) => nombre.toUpperCase()).toList();
+  print('NewList: ${newList}');
 }
