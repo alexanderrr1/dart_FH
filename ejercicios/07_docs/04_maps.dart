07b94483676b1cee7fecbebf4d40a8676c15a45c
Alexander Reineck <areineck@ohgiftcard.com>
2022-10-17 09:38:49 -0300

    Método Map de los mapas

--- ejercicios/07_docs/04_maps.dart
+++ ejercicios/07_docs/04_maps.dart
@@ -27,12 +27,20 @@ void main(List<String> args) {
   print('AddAll: ${persona}');
 
   // Remove
-  persona.remove('pais');
-  print('Remove: ${persona}');
+  // persona.remove('pais');
+  // print('Remove: ${persona}');
 
   // RemoveWhere
-  persona.removeWhere((key, value) => key != 'nombre');
-  print('RemoveWhere: ${persona}');
+  // persona.removeWhere((key, value) => key != 'nombre');
+  // print('RemoveWhere: ${persona}');
 
+  // ForEach
+  persona.forEach((key, value) { print('key: $key value: $value'); });
+
+  // Map
+  final nuevoMapa = persona.map((key, value) {
+    return MapEntry(key, value.toString().toUpperCase());
+  });
+  print('Persona map: $nuevoMapa');
 
 }
