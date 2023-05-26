e59eb7931a36791ac29991c430c0a19175fc9294
Alexander Reineck <areineck@ohgiftcard.com>
2023-05-26 18:41:59 -0300

    Extraer y usar el body de una peticion HTTP

--- proyecto/paquetes/bin/main.dart
+++ proyecto/paquetes/bin/main.dart
@@ -1,5 +1,18 @@
-import 'package:paquetes/paquetes.dart' as paquetes;
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
-  print('Hello world: ${paquetes.calculate()}!');
+  final url = Uri.parse('https://reqres.in/api/users?page=2');
+
+  http.get(url).then((res) {
+    // print(res);
+
+    final body = jsonDecode(res.body);
+
+    // print(body);
+    print('page: ${body['page']}');
+    print("per_page: ${body['per_page']}");
+    print("id del tercer elemento: ${body['data'][2]['id']}");
+  });
 }
