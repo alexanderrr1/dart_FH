765e8cc7862efefd4dc86e6929a2c2693a368674
Alexander Reineck <areineck@ohgiftcard.com>
2023-05-26 19:00:10 -0300

    Mapear la respuesta a una instancia de clase

--- proyecto/paquetes/bin/main.dart
+++ proyecto/paquetes/bin/main.dart
@@ -1,18 +1,25 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:paquetes/classes/reqres_respuesta.dart';
 
 void main(List<String> arguments) {
+
   final url = Uri.parse('https://reqres.in/api/users?page=2');
 
   http.get(url).then((res) {
-    // print(res);
+    final resReqRes = reqResRespuestaFromJson(res.body);
 
-    final body = jsonDecode(res.body);
+    //   final body = jsonDecode(res.body);
 
-    // print(body);
-    print('page: ${body['page']}');
-    print("per_page: ${body['per_page']}");
-    print("id del tercer elemento: ${body['data'][2]['id']}");
+    //   print('page: ${body['page']}');
+    //   print("per_page: ${body['per_page']}");
+    //   print("id del tercer elemento: ${body['data'][2]['id']}");
+
+    print('page: ${resReqRes.page}');
+    print("per_page: ${resReqRes.perPage}");
+    print("id del tercer elemento: ${resReqRes.data[2].id}");
+
   });
+  
 }
